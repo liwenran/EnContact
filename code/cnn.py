0eba414be7356a9ce7f913a664d06e11bb0cc681
liwenran <26700222+liwenran@users.noreply.github.com>
2019-06-17 19:20:31 -0700

    model

--- code/cnn.py
+++ code/cnn.py
@@ -23,10 +23,6 @@ from keras.models import load_model
 from keras.engine.topology import Layer, InputSpec
 from keras import initializations
 
-RESIZED_ENHANCER_LEN = 2000
-RESIZED_PROMOTER_LEN = 2000
-SEQ_LEN = 2000
-
 
 # Attention GRU network
 class AttLayer(Layer):
@@ -57,18 +53,18 @@ class AttLayer(Layer):
 
 def model_def():
     drop_rate = 0.5 
-    conv_enhancer_seq = Sequential()
-    conv_enhancer_seq.add(Convolution2D(1024, 4, 40, activation = 'relu', border_mode = 'valid',
-                                        dim_ordering = 'th', input_shape = (1, 4, RESIZED_ENHANCER_LEN)))
-    conv_enhancer_seq.add(MaxPooling2D(pool_size = (1, 20), border_mode = 'valid', dim_ordering = 'th'))
-    conv_enhancer_seq.add(Reshape((1024, 98)))
-    conv_promoter_seq = Sequential()
-    conv_promoter_seq.add(Convolution2D(1024, 4, 40, activation = 'relu', border_mode = 'valid',
-                                        dim_ordering = 'th', input_shape = (1, 4, RESIZED_PROMOTER_LEN)))
-    conv_promoter_seq.add(MaxPooling2D(pool_size = (1, 20), border_mode = 'valid', dim_ordering = 'th'))
-    conv_promoter_seq.add(Reshape((1024, 98)))
+    conv_enhancer1_seq = Sequential()
+    conv_enhancer1_seq.add(Convolution2D(1024, 4, 40, activation = 'relu', border_mode = 'valid',
+                                        dim_ordering = 'th', input_shape = (1, 4, SEQ_LEN)))
+    conv_enhancer1_seq.add(MaxPooling2D(pool_size = (1, 20), border_mode = 'valid', dim_ordering = 'th'))
+    conv_enhancer1_seq.add(Reshape((1024, 98)))
+    conv_enhancer2_seq = Sequential()
+    conv_enhancer2_seq.add(Convolution2D(1024, 4, 40, activation = 'relu', border_mode = 'valid',
+                                        dim_ordering = 'th', input_shape = (1, 4, SEQ_LEN)))
+    conv_enhancer2_seq.add(MaxPooling2D(pool_size = (1, 20), border_mode = 'valid', dim_ordering = 'th'))
+    conv_enhancer2_seq.add(Reshape((1024, 98)))
     merged = Sequential()
-    merged.add(Merge([conv_enhancer_seq, conv_promoter_seq], mode = 'concat'))
+    merged.add(Merge([conv_enhancer1_seq, conv_enhancer2_seq], mode = 'concat'))
     merged.add(Permute((2, 1)))
     merged.add(BatchNormalization())
     merged.add(Dropout(drop_rate))
@@ -85,61 +81,6 @@ def model_def():
     model.add(Dense(1, activation = 'sigmoid'))
     return model
 
-def evaluate_performance(y_truth, y_pred, p_y_given_x):
-    TP = float(sum((y_truth == 1) & (y_pred == 1)))
-    FP = float(sum((y_truth == 0) & (y_pred == 1)))
-    FN = float(sum((y_truth == 1) & (y_pred == 0)))
-    TN = float(sum((y_truth == 0) & (y_pred == 0)))
-    ACC = (TP + TN) / (TP + FP + FN + TN)
-    BER = 0.5 * (FN / (FN + TP + 1e-7) + FP / (FP + TN + 1e-7))
-    P = TP / (TP + FP + 1e-7)
-    R = TP / (TP + FN + 1e-7)
-    F1 = 2 * P * R / (P + R + 1e-7)
-    try:
-        MCC = (TP * TN - FP * FN) / ((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN)) ** 0.5
-    except:
-        MCC = 0.0
-    from sklearn.metrics import roc_auc_score, average_precision_score
-    from sklearn.metrics import roc_curve, precision_recall_curve
-    auROC = roc_auc_score(y_truth, p_y_given_x)
-    auPR = average_precision_score(y_truth, p_y_given_x)
-    fpr, tpr, _ = roc_curve(y_truth, p_y_given_x)
-    precision, recall, _ = precision_recall_curve(y_truth, p_y_given_x)
-    return (ACC, BER, MCC, auROC, auPR, fpr, tpr, precision, recall, P, R, F1)
-
-def evaluate(Y_val, y_pred, y_score):
-    acc   = metrics.accuracy_score(Y_val, y_pred)
-    print('Test acc: ', acc)
-    precision = metrics.precision_score(Y_val, y_pred)
-    print('Test precision: ', precision)
-    recall = metrics.recall_score(Y_val, y_pred)
-    print('Test recall: ', recall)
-    f1    = metrics.f1_score(Y_val, y_pred)
-    print('Test f1: ', f1) 
-    mcc   = metrics.matthews_corrcoef(Y_val, y_pred)
-    print('Test mcc: ', mcc)
-    fpr, tpr, thresholds = metrics.roc_curve(Y_val, y_score, pos_label=1)
-    fpr = np.asarray(fpr, dtype="str")
-    tpr = np.asarray(tpr, dtype="str")
-    thresholds = np.asarray(thresholds, dtype="str")
-    auroc = metrics.roc_auc_score(Y_val, y_score)
-    print('Test auroc: ', auroc)
-    auprc = metrics.average_precision_score(Y_val, y_score)
-    print('Test auprc: ', auprc)
-
-    fout=open("./models/"+timestamp+"/hist.txt","a")
-    fout.write('Test accuracy: '+str(acc)+'\n')
-    fout.write('Test f1 score: '+str(f1)+'\n')
-    fout.write('Test precision: '+str(precision)+'\n')
-    fout.write('Test recall: '+str(recall)+'\n')
-    fout.write('Test mcc: '+str(mcc)+'\n')
-    fout.write('Test auroc: '+str(auroc)+'\n')
-    fout.write('Test auprc: '+str(auprc)+'\n')
-    fout.write('fpr'+'\t'+ '\t'.join(fpr.tolist())+'\n')
-    fout.write('tpr'+'\t'+ '\t'.join(tpr.tolist())+'\n')
-    fout.write('thresholds'+'\t'+ '\t'.join(thresholds.tolist())+'\n')
-    fout.close()
-
 def f1(y_true, y_pred):
     TP = K.sum(K.equal(y_true, 1) & K.equal(K.round(y_pred), 1))
     FP = K.sum(K.equal(y_true, 0) & K.equal(K.round(y_pred), 1))
@@ -151,33 +92,31 @@ def f1(y_true, y_pred):
     F1 = 2 * P * R / (P + R + K.epsilon())
     return F1
 
-def train_and_evaluate(model):
+def training(model):
     print('Loading data...')
-    enhancer_shape = (-1, 1, RESIZED_ENHANCER_LEN, 4)
-    promoter_shape = (-1, 1, RESIZED_PROMOTER_LEN, 4)
-    enhancer_D = np.load(cell+'/enhancer1_B.npz')
-    promoter_D = np.load(cell+'/enhancer2_B.npz')
+	SEQ_LEN = 2000
+    enhancer_shape = (-1, 1, SEQ_LEN, 4)
+    seq1 = np.load(cell+'/enhancer1.npz')
+    seq2 = np.load(cell+'/enhancer2.npz')
     #
-    n_D = enhancer_D['label'].shape[0]
-    np.random.seed(n_D)
-    rand_index_D = range(0, n_D)
-    np.random.shuffle(rand_index_D)
-    label_D = enhancer_D['label'][rand_index_D]
-    enhancer_seq_D = enhancer_D['sequence'].astype('float32').reshape(enhancer_shape).transpose(0, 1, 3, 2)[rand_index_D]
-    promoter_seq_D = promoter_D['sequence'].astype('float32').reshape(promoter_shape).transpose(0, 1, 3, 2)[rand_index_D]
+    label = seq1['label'].shape[0]
+    np.random.seed(label)
+    rand_index = range(0, label)
+    np.random.shuffle(rand_index)
+    label = seq1['label'][rand_index]
+    seq1 = seq1['sequence'].astype('float32').reshape(enhancer_shape).transpose(0, 1, 3, 2)[rand_index]
+    seq2 = seq2['sequence'].astype('float32').reshape(enhancer_shape).transpose(0, 1, 3, 2)[rand_index]
     
     ## Train model
     model.compile(loss = 'binary_crossentropy',
                   optimizer = optimizers.Adam(lr = 0.00001),
                   metrics = ['acc', f1])
-    filename = cell+'/models/best_model_seq_'+Type+'.h5'
+    filename = cell+'/models/best_model.h5'
     modelCheckpoint = ModelCheckpoint(filename, monitor = 'val_acc', save_best_only = True, mode = 'max')
-    model.fit([enhancer_seq_D, promoter_seq_D], label_D, nb_epoch = 50, batch_size = 100,
+    model.fit([seq1, seq2], label, nb_epoch = 50, batch_size = 100,
               validation_split = 0.1, callbacks = [modelCheckpoint])
 
 """ MAIN """
+cell = argv[1]
 model = model_def()
-os.system('mkdir -p '+cell+'/models-'+DEF)
-train_and_evaluate(model)
-
-
+training(model)
